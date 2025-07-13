96744c2848f191cab892b9f1669992fe3f5b15bc
Valeeriy Vershkov <99196723+Drablow@users.noreply.github.com>
2025-07-13 19:31:28 +0400

    Update main.py read_root

--- catalog-movie/main.py
+++ catalog-movie/main.py
@@ -1,3 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
-app = FastAPI()
+app = FastAPI(
+    title="Catalog Movie",
+)
+
+
+@app.get("/")
+def read_root(request: Request, name: str = "World"):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"Hello, {name}!",
+        "docs": str(docs_url),
+    }
